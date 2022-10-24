5d079080986b1a1c26e8c1810361e7ad2c5905df
Yoga Septian Nugraha <82901000+Yogasn839@users.noreply.github.com>
2022-10-24 11:13:45 +0700

    Add files via upload

---
+++ rentalmobil.sql
@@ -0,0 +1,7 @@
+id_peminjaman,nama_mobil,tanggal_peminjaman,tanggal_pengembalian,status_peminjaman
+1,Mitsubishi Lancer Evolution,2022-05-01,,Dipakai
+2,Toyota Supra,2022-05-02,,Dipakai
+3,Honda NSX1,2022-05-05,,Dipakai
+4,BMW X2,2022-05-07,,Dipakai
+5,Mercedes Benz S Class,2022-08-11,,Dipakai
+7,Subaru RXZ,2022-03-11,2022-09-12,Dikembalikan
---
+++ rentalmobil_new.sql
@@ -0,0 +1,7 @@
+id_peminjaman,nama_mobil,tanggal_peminjaman,tanggal_pengembalian,status_peminjaman
+1,Mazda CX2,2022-07-11,,Dipakai
+2,Mitsubishi Lancer Evolution,2022-05-12,,Dipakai
+3,Hyundai Creta,2022-02-11,,Dipakai
+4,Honda BRV,2022-04-10,,Dipakai
+5,BMW X5,2022-09-29,,Dipakai
+6,Daihatsu Xenia,2022-11-19,2022-12-30,Dikembalikan
